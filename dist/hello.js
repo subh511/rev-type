9730b9041013f61fca1e5f2ad151787f9a5072bb
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-04-29 18:27:48 +0530

    first commit

---
+++ dist/hello.js
@@ -0,0 +1,10 @@
+"use strict";
+// interface User{
+//     firstname: string,
+//     lastname: string,
+//     age: number
+// }
+function sayHello(firstname) {
+    console.log("hello " + firstname);
+}
+sayHello("Samrat");
---
+++ src/hello.ts
@@ -0,0 +1,14 @@
+
+// interface User{
+//     firstname: string,
+//     lastname: string,
+//     age: number
+// }
+
+
+function sayHello(firstname: string){
+
+    console.log("hello " + firstname )
+}
+
+sayHello("Samrat");
